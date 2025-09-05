0e2327feb32fcfb282393018f35b4f95a08915db
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-09-05 11:40:40 +0600

    structure ready

---
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div className='text-center w-3/4 mx-auto'>
+            <h1 className='text-3xl text-center text-red-300'>Hey, Homie</h1>
+            <h2 className='text-6xl text-center text-red-600'>Error 404!</h2>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Footer/Footer';
 
 const Root = () => {
     return (
         <div>
-            <h1>Welcome Homie</h1>
+            <h1 className='text-red-300 text-8xl'>Welcome Homie</h1>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
